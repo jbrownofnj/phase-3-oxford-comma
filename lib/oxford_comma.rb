6e9cc68b7214e9cfbd1a7e6b8eff5f355c97c4bd
John Brown <johnbrownsinbox@gmail.com>
2022-04-06 10:34:12 -0400

    commit tree 9e9841d59af72d813ce8e5a71a85b72757a93690

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
+require "pry"
+testArray1 = ["One"]
+testArray2 = ["Two","Three"]
+testArray3 = ["Four","Five"]
 def oxford_comma(array)
-
+    returnWord=""
+    array.each {|word| 
+        if(word!=array[-1]||array.length==1)
+        returnWord=returnWord+word+","
+        end
+        
+    }
+    array.length>1?returnString=returnWord.slice(0...returnWord.length)+"and"+array[-1]:returnWord=array[0]
+    returnWord
 end
+oxford_comma(testArray1)
